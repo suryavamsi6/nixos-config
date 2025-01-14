29b45e70d0a9711a937188d18c26cde138781c8c
Surya Vamsi <d.suryavamsi@gmail.com>
2025-01-14 19:35:19 +0530

    Moved to config folder

---
+++ homeManagerModules/dotfiles/hyprland/config/animations.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}:
+{
+  animations = {
+        enabled = true;
+        bezier = [
+            "easeOutQuint,0.23,1,0.32,1"
+            "easeInOutCubic,0.65,0.05,0.36,1"
+            "linear,0,0,1,1"
+            "almostLinear,0.5,0.5,0.75,1.0"
+            "quick,0.15,0,0.1,1"
+            "overshot,0.13,0.99,0.29,1.1"
+            "shot,0.2,1.0,0.2,1.0"
+            "swipe,0.6,0.0,0.2,1.05"
+            "linear,0.0,0.0,1.0,1.0"
+            "progressive,1.0,0.0,0.6,1.0"
+        ];
+        animation = [
+            "windows,1,6,shot,slide"
+            "workspaces,1,6,overshot,slidevert"
+            "fade,1,4,linear"
+            "border,1,4,linear"
+            "borderangle,1,180,linear"
+        ];
+    };
+}
--- homeManagerModules/dotfiles/hyprland/hyprland.nix
+++ homeManagerModules/dotfiles/hyprland/hyprland.nix
@@ -1,14 +1,15 @@
 {pkgs, inputs,  ...}:
 let
-    autostart = import ./autostart.nix { inherit pkgs; };
-    keybind = import ./keybind.nix { inherit pkgs; };
-    defaultApps = import ./defaultApps.nix { inherit pkgs; };
+    autostart = import ./config/autostart.nix { inherit pkgs; };
+    keybind = import ./config/keybinds.nix { inherit pkgs; };
+    defaultApps = import ./config/defaultApps.nix { inherit pkgs; };
+    animations = import ./config/animations.nix { inherit pkgs; };
 in
 {   
     wayland.windowManager.hyprland = {
         enable = true;
         package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-        settings = autostart // keybind // defaultApps // {
+        settings = autostart // keybind // defaultApps // animations // {
 
             "$mainMod" = "SUPER";
             
@@ -81,31 +82,6 @@ in
 
             };
 
-            animations = {
-                enabled = true;
-
-                bezier = [
-                    "easeOutQuint,0.23,1,0.32,1"
-                    "easeInOutCubic,0.65,0.05,0.36,1"
-                    "linear,0,0,1,1"
-                    "almostLinear,0.5,0.5,0.75,1.0"
-                    "quick,0.15,0,0.1,1"
-                    "overshot,0.13,0.99,0.29,1.1"
-                    "shot,0.2,1.0,0.2,1.0"
-                    "swipe,0.6,0.0,0.2,1.05"
-                    "linear,0.0,0.0,1.0,1.0"
-                    "progressive,1.0,0.0,0.6,1.0"
-                ];
-
-                animation = [
-                    "windows,1,6,shot,slide"
-                    "workspaces,1,6,overshot,slidevert"
-                    "fade,1,4,linear"
-                    "border,1,4,linear"
-                    "borderangle,1,180,linear"
-                ];
-
-            };
 
             dwindle = {
                 pseudotile = true;
