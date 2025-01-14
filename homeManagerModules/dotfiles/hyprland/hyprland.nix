177cb9a8e7fd98779c661d09b1fb16ba90319316
Surya Vamsi <d.suryavamsi@gmail.com>
2025-01-14 17:42:58 +0530

    Modularise

--- homeManagerModules/dotfiles/hyprland/hyprland.nix
+++ homeManagerModules/dotfiles/hyprland/hyprland.nix
@@ -1,12 +1,13 @@
 {pkgs, inputs,  ...}:
 let
     autostart = import ./autostart.nix { inherit pkgs; };
+    keybind = import ./keybind.nix { inherit pkgs; };
 in
 {   
     wayland.windowManager.hyprland = {
         enable = true;
         package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-        settings = autostart // {
+        settings = autostart // keybind //{
 
             "$mainMod" = "SUPER";
             
@@ -151,76 +152,7 @@ in
                 sensitivity = -0.5;
             };
 
-            bind = [
-                "$mainMod, return, exec, $terminal"
-                "$mainMod, C, killactive,"
-                "$mainMod, B, exec, $browser"
-                "$mainMod, M, exit,"
-                "$mainMod, E, exec, $fileManager"
-                "$mainMod, V, togglefloating,"
-                "$mainMod, space, exec, $menu"
-                "$mainMod, P, pseudo,"
-                "$mainMod, J, togglesplit,"
 
-                "$mainMod, left, movefocus, l"
-                "$mainMod, right, movefocus, r"
-                "$mainMod, up, movefocus, u"
-                "$mainMod, down, movefocus, d"
-
-                "$mainMod SHIFT, l, swapwindow, l"
-                "$mainMod SHIFT, r, swapwindow, r"
-                "$mainMod SHIFT, u, swapwindow, u"
-                "$mainMod SHIFT, d, swapwindow, d"
-
-                "$mainMod, 1, workspace, 1"
-                "$mainMod, 2, workspace, 2"
-                "$mainMod, 3, workspace, 3"
-                "$mainMod, 4, workspace, 4"
-                "$mainMod, 5, workspace, 5"
-                "$mainMod, 6, workspace, 6"
-                "$mainMod, 7, workspace, 7"
-                "$mainMod, 8, workspace, 8"
-                "$mainMod, 9, workspace, 9"
-                "$mainMod, 0, workspace, 10"
-
-                "$mainMod SHIFT, 1, movetoworkspace, 1"
-                "$mainMod SHIFT, 2, movetoworkspace, 2"
-                "$mainMod SHIFT, 3, movetoworkspace, 3"
-                "$mainMod SHIFT, 4, movetoworkspace, 4"
-                "$mainMod SHIFT, 5, movetoworkspace, 5"
-                "$mainMod SHIFT, 6, movetoworkspace, 6"
-                "$mainMod SHIFT, 7, movetoworkspace, 7"
-                "$mainMod SHIFT, 8, movetoworkspace, 8"
-                "$mainMod SHIFT, 9, movetoworkspace, 9"
-                "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-                "$mainMod, S, togglespecialworkspace, magic"
-                "$mainMod SHIFT, S, movetoworkspace, special:magic"
-
-                "$mainMod, mouse_down, workspace, e+1"
-                "$mainMod, mouse_up, workspace, e-1"
-            ];
-
-            bindm = [
-                "$mainMod, mouse:272, movewindow"
-                "$mainMod, mouse:273, resizewindow"
-            ];
-
-            bindel = [
-                ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-                ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-                ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-                ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-                ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-                ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
-            ];
-
-            bindl = [
-                ", XF86AudioNext, exec, playerctl next"
-                ", XF86AudioPause, exec, playerctl play-pause"
-                ", XF86AudioPlay, exec, playerctl play-pause"
-                ", XF86AudioPrev, exec, playerctl previous"
-            ];
 
             windowrulev2 = [
                 "suppressevent maximize, class:.*"
