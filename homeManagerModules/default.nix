e4dd267d4f7dfaa5840b01b33c768622a916eb6e
Surya Vamsi <d.suryavamsi@gmail.com>
2025-01-21 09:47:24 +0530

    Add walker config

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -5,6 +5,7 @@
     ./cliModules/sh.nix
     ./dotfiles/hyprland/hyprland.nix
     ./dotfiles/hyprpanel/hyprpanel.nix
-#    ./systemModules/kanshi.nix
+    ./systemModules/walker.nix
+    #    ./systemModules/kanshi.nix
   ];
 }
