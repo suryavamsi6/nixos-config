1a89d879b0c7d4aa1e3d11084e34b1e8a1e1c212
Surya Vamsi <d.suryavamsi@gmail.com>
2025-01-27 17:29:31 +0530

    Switch back open nvidia drivers

--- systems/x86_64-linux/hyprland/configuration.nix
+++ systems/x86_64-linux/hyprland/configuration.nix
@@ -129,22 +129,9 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "565.77";
-      sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
-      sha256_aarch64 = "sha256-LSAYUnhfnK3rcuPe1dixOwAujSof19kNOfdRHE7bToE=";
-      openSha256 = "sha256-Fxo0t61KQDs71YA8u7arY+503wkAc1foaa51vi2Pl5I=";
-      settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
-      persistencedSha256 = "sha256-wnDjC099D8d9NJSp9D0CbsL+vfHXyJFYYgU3CwcqKww=";
-      patchesOpen = [
-        (pkgs.fetchpatch {
-          url = "https://gitlab.manjaro.org/packages/extra/linux613-extramodules/nvidia/-/raw/main/kernel-6.13.patch";
-          hash = "sha256-C21kymMJT/C2kuUXnOyqwLPzpas4/6CAXTllL24i8ic=";
-        })
-      ];
-    };
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   hardware.opengl = {
